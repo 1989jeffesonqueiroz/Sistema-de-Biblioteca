0233a2e93feb99cd6d2df06a6cc38f46f7241d60
1989jeffesonqueiroz <58812763+1989jeffesonqueiroz@users.noreply.github.com>
2020-06-07 18:41:54 -0300

    Add files via upload

---
+++ Versão 6.0/model/Login.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Login {
+    
+    private int id_login;
+    private String login;
+    private String senha;
+    
+    public Login (){
+        
+    }
+
+    public Login(int id_login, String login, String senha) {
+        this.id_login = id_login;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId_login() {
+        return id_login;
+    }
+
+    public void setId_login(int id_login) {
+        this.id_login = id_login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+    
+}
